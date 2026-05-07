3d7e534004d2602ef6e2a7a3e21c3e00cbce4cde
abboskhanme <abboshkhan.me@gmail.com>
2026-05-07 08:57:15 +0500

    first deploy

---
+++ config.js
@@ -0,0 +1,7 @@
+// ══════════════════════════════════════
+// SUPABASE CONFIG
+// Quyidagi qiymatlarni o'zingiznikiga almashtiring
+// ══════════════════════════════════════
+
+const SUPABASE_URL = 'https://ssprwlksgukjjiwjcdaa.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Ii52-3sHBgs_NVOQluJqEw_3fIV61e7';
